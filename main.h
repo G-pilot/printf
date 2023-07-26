ec68cbb30e828a1ec9e08cfdbef148f0714c38a4
G-pilot <gregoryogechukwu7@gmail.com>
2023-07-26 08:41:06 -0700

    update

--- main.h
+++ main.h
@@ -20,13 +20,12 @@ int _printf(const char *format, ...);
 int put_str(char *string);
 int put_int(int integer);
 int _putchar(char c);
-ssize_t write(int fd, const void *buf size_t count);
+ssize_t write(int fd, const void *buf, size_t count);
 void *malloc(size_t size);
 void free(void *ptr);
-void va_start(va_list ap, last_arg);
+void va_start(va_list ap, ...);
 void va_end(va_list ap);
 void va_copy(va_list dest, va_list src);
-type va_arg(va_list ap, type);
 
 
 #endif
